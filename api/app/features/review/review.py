5382536858926b2747ddbe021a6f4e62166ec5dc
KamilisJ <kamjon@ktu.lt>
2022-05-03 17:30:45 +0300

    Corrected mask review

--- api/app/features/review/review.py
+++ api/app/features/review/review.py
@@ -23,13 +23,23 @@ class MaskImage:
 
 __FACE_MESH = mp.solutions.face_mesh.FaceMesh()
 __DEFAULT_MASK_POINTS = [
-    CoordinateIndex(120, 345, 93),
+    CoordinateIndex(120, 345, 234),
+    CoordinateIndex(160, 345, 227), #
+    CoordinateIndex(218, 368, 117), #
     CoordinateIndex(275, 385, 197),
-    CoordinateIndex(423, 345, 323),
-    CoordinateIndex(423, 245, 288),
-    CoordinateIndex(305, 170, 400),
-    CoordinateIndex(245, 173, 140),
-    CoordinateIndex(120, 255, 172)
+    CoordinateIndex(329, 368, 346), #
+    CoordinateIndex(383, 345, 447), #
+    CoordinateIndex(423, 345, 454),
+    CoordinateIndex(423, 295, 361),
+    CoordinateIndex(423, 245, 397),
+    CoordinateIndex(365, 207, 379),
+    CoordinateIndex(305, 173, 400),
+    CoordinateIndex(275, 165, 152),
+    CoordinateIndex(245, 173, 176),
+    CoordinateIndex(183, 205, 150),
+    CoordinateIndex(120, 255, 172),
+    CoordinateIndex(120, 300, 132),
+
 ]
 
 
@@ -37,12 +47,14 @@ def __raw_image_to_cv2(raw_image: bytes):
     return cv2.imdecode(np.frombuffer(raw_image, dtype=np.uint8), flags=1)
 
 
-def __try_create_review_image(raw_face_image: bytes, mask: MaskImage):
+def __try_create_review_image(raw_face_image: bytes, mask: MaskImage, indexes_triangles,):
     # # Visualize the points
-    # for i in range(0, np.size(mask_points)):
-    #     x = mask_points[i].x
-    #     y = mask_points[i].y
-    #     cv2.circle(mask_img, (x, y), 2, (255, 0, 0), -1)
+    for i in range(0, np.size(__DEFAULT_MASK_POINTS)):
+        x = __DEFAULT_MASK_POINTS[i].x
+        y = __DEFAULT_MASK_POINTS[i].y
+        cv2.circle(mask.img, (x, 548- y), 2, (255, 0, 0), -1)
+
+    # return cv2.imencode('.png', mask.img)[1]
 
     image = __raw_image_to_cv2(raw_face_image)
     height, width, _ = image.shape
@@ -79,29 +91,30 @@ def __try_create_review_image(raw_face_image: bytes, mask: MaskImage):
     # cv2.polylines(mask_img, [convexhull_mask], True, (255, 0, 0), 1)
     cv2.fillConvexPoly(np.zeros_like(mask.img_gray), convexhull_mask, 255)
 
-    # Delaunay triangulation
-    rect = cv2.boundingRect(convexhull)
-    subdiv = cv2.Subdiv2D(rect)
-    subdiv.insert(landmarks_points)
-    triangles = subdiv.getTriangleList()
-    triangles = np.array(triangles, dtype=np.int32)
+    if len(indexes_triangles) == 0:
+        # Delaunay triangulation
+        rect = cv2.boundingRect(convexhull)
+        subdiv = cv2.Subdiv2D(rect)
+        subdiv.insert(landmarks_points)
+        triangles = subdiv.getTriangleList()
+        triangles = np.array(triangles, dtype=np.int32)
 
-    indexes_triangles = []
-    for tr_index in triangles:
-        pt1 = (tr_index[0], tr_index[1])
-        pt2 = (tr_index[2], tr_index[3])
-        pt3 = (tr_index[4], tr_index[5])
+        for tr_index in triangles:
+            pt1 = (tr_index[0], tr_index[1])
+            pt2 = (tr_index[2], tr_index[3])
+            pt3 = (tr_index[4], tr_index[5])
 
-        index_pt1 = np.where((points == pt1).all(axis=1))[0][0]
+            index_pt1 = np.where((points == pt1).all(axis=1))[0][0]
 
-        index_pt2 = np.where((points == pt2).all(axis=1))[0][0]
+            index_pt2 = np.where((points == pt2).all(axis=1))[0][0]
 
-        index_pt3 = np.where((points == pt3).all(axis=1))[0][0]
+            index_pt3 = np.where((points == pt3).all(axis=1))[0][0]
 
-        if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
-            triangle = [index_pt1, index_pt2, index_pt3]
-            indexes_triangles.append(triangle)
-    # print(indexes_triangles)
+            if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
+                triangle = [index_pt1, index_pt2, index_pt3]
+                indexes_triangles.append(triangle)
+
+        # print(indexes_triangles)
 
     # Triangulation of mask and face
     for tr_index in indexes_triangles:
@@ -169,20 +182,21 @@ def __try_create_review_image(raw_face_image: bytes, mask: MaskImage):
     image_face_mask = cv2.bitwise_not(image_head_mask)
 
     image_head_noface = cv2.bitwise_and(image, image, mask=image_face_mask)
-    cv2.add(image_head_noface, image_new_face)
+    result = cv2.add(image_head_noface, image_new_face)
 
     # (x, y, w, h) = cv2.boundingRect(convexhull)
     # center_face2 = (int((x + x + w) / 2), int((y + y + h) / 2))
 
     # seamlessclone = cv2.seamlessClone(result, image, image_head_mask, center_face2, cv2.MIXED_CLONE)
 
-    return cv2.imencode('.png', image)[1]
+    return cv2.imencode('.png', result)[1]
 
 
 def preprocess_mask_image(raw_mask_image: bytes):
-    mask_img = __raw_image_to_cv2(raw_mask_image)
+    mask_img = cv2.resize(__raw_image_to_cv2(raw_mask_image), (548, 548))
     mask_img_gray = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
     mask_height, mask_width, _ = mask_img.shape
+    print(mask_height, mask_width)
     mask_points = [
         CoordinateIndex(x=p.x, y=mask_height - p.y, index=p.index)
         for p in __DEFAULT_MASK_POINTS
@@ -198,8 +212,8 @@ def preprocess_mask_image(raw_mask_image: bytes):
     )
 
 
-def create_review_image(raw_face_image: bytes, mask: MaskImage):
+def create_review_image(raw_face_image: bytes, mask: MaskImage, indexes_triangles,):
     try:
-        return __try_create_review_image(raw_face_image, mask)
+        return __try_create_review_image(raw_face_image, mask, indexes_triangles,)
     except Exception:
         return raw_face_image
